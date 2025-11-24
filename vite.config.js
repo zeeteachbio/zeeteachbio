0d19287a9ebc7a5a47d5a3ba818ea7038a6309d2
zeeteachbio <bio.nerd.zeeshan@gmail.com>
2025-11-24 21:39:18 +0500

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
     // IMPORTANT: Replace 'REPO_NAME' with your GitHub repository name
     // e.g. if your repo is 'https://github.com/user/my-project', use '/my-project/'
     // If you are deploying to https://<USERNAME>.github.io/, set base to '/'
-    base: '/zeeteachbio/',
+    base: '/',
 })
