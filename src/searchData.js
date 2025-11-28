b05c1f9f1508691f87d351ebf71b0d0fa3a79118
zeeteachbio <bio.nerd.zeeshan@gmail.com>
2025-11-28 16:43:11 +0500

    Add Introduction to Biology to search index

--- src/searchData.js
+++ src/searchData.js
@@ -9,5 +9,16 @@ export const searchIndex = [
         "views": 0,
         "comments": 0,
         "thumbnail": null
+    },
+    {
+        "title": "Introduction to Biology",
+        "url": "/introduciton-to-biology.html",
+        "excerpt": "Introduction to Biology - STB Class 9 (1. Introduction to Biology) notes.",
+        "category": "STB Class 9",
+        "chapter": "1. Introduction to Biology",
+        "date": "2025-11-28T11:43:11.600Z",
+        "views": 0,
+        "comments": 0,
+        "thumbnail": null
     }
 ];
