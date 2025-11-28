16cc23344f3a7533cdbd64b8dafd4d63d9d213ed
zeeteachbio <bio.nerd.zeeshan@gmail.com>
2025-11-28 08:50:58 +0500

    Add Zoology to search index

--- src/searchData.js
+++ src/searchData.js
@@ -1,1 +1,13 @@
-export const searchIndex = [];
+export const searchIndex = [
+    {
+        "title": "Zoology",
+        "url": "/article-hi-new-topic.html",
+        "excerpt": "Zoology - Class 9 (1. Introduction to Biology) notes.",
+        "category": "Class 9",
+        "chapter": "1. Introduction to Biology",
+        "date": "2025-11-28T03:50:56.511Z",
+        "views": 0,
+        "comments": 0,
+        "thumbnail": null
+    }
+];
