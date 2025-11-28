eff1a48d25d649c9dd489adfaaac1cfc47c0a7e9
zeeteachbio <bio.nerd.zeeshan@gmail.com>
2025-11-28 14:41:00 +0500

    Remove article-hi-new-topic.html from search index

--- src/searchData.js
+++ src/searchData.js
@@ -1,16 +1,5 @@
 export const searchIndex = [
     {
-        "title": "Zoology",
-        "url": "/article-hi-new-topic.html",
-        "excerpt": "Zoology - Class 9 (1. Introduction to Biology) notes.",
-        "category": "Class 9",
-        "chapter": "1. Introduction to Biology",
-        "date": "2025-11-28T03:50:56.511Z",
-        "views": 0,
-        "comments": 0,
-        "thumbnail": null
-    },
-    {
         "title": "Transport",
         "url": "/article-testing-article-api.html",
         "excerpt": "Transport - AKUEB Class 11 (13. Transport) notes.",
