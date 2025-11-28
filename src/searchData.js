2bf8ad5976d9da70bb27159ed69e8b93841b7610
zeeteachbio <bio.nerd.zeeshan@gmail.com>
2025-11-28 09:59:06 +0500

    Add Transport to search index

--- src/searchData.js
+++ src/searchData.js
@@ -9,5 +9,16 @@ export const searchIndex = [
         "views": 0,
         "comments": 0,
         "thumbnail": null
+    },
+    {
+        "title": "Transport",
+        "url": "/article-testing-article-api.html",
+        "excerpt": "Transport - AKUEB Class 11 (13. Transport) notes.",
+        "category": "AKUEB Class 11",
+        "chapter": "13. Transport",
+        "date": "2025-11-28T04:59:04.555Z",
+        "views": 0,
+        "comments": 0,
+        "thumbnail": null
     }
 ];
